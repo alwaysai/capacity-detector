026a92b3ee9346abe6dc814d9be1615d0e10b6ba
Eric VanBuhler <eric@alwaysai.co>
2023-06-21 16:23:12 -0700

    Remove OPENVINO engine

--- alwaysai.py
+++ alwaysai.py
@@ -4,28 +4,13 @@ import alwaysai_configs
 import copy
 
 
-def engine():
-    """Switch Engine modes if an Intel accelerator is available"""
-    if is_accelerator_available() is True:
-        return edgeiq.Engine.DNN_OPENVINO
-    return edgeiq.Engine.DNN
-
-
-def is_accelerator_available():
-    """Detect if an Intel Neural Compute Stick accelerator is attached"""
-    if edgeiq.find_ncs2():
-        return True
-    return False
-
-
 def object_detector(model, should_log=True):
     """Return an object detector for a given model"""
     if model is None:
         raise Exception(
             "alwaysai.py: object_detector: model name parameter not found")
     od = edgeiq.ObjectDetection(model)
-    e = engine()
-    od.load(e)
+    od.load(edgeiq.Engine.DNN)
     if should_log is True:
         print("alwaysai.py: object_detector: Engine: {}".format(od.engine))
         print("alwaysai.py: object_detector: Accelerator: {}\n".format(
